415354408cbe49a72a215a8e6006d9ab69f209cb
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2021-02-28 22:34:14 -0600

    create task

--- src/main/java/com/gabriellazar/projectmanagement/controllers/WebController.java
+++ src/main/java/com/gabriellazar/projectmanagement/controllers/WebController.java
@@ -3,10 +3,7 @@ package com.gabriellazar.projectmanagement.controllers;
 import com.gabriellazar.projectmanagement.dto.UserDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
 import org.springframework.web.bind.annotation.ModelAttribute;
-
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -50,7 +47,20 @@ public class WebController {
         return "/fragments/navbar";
     }
 
+    @RequestMapping("/project-status")
+    public String getProjectStatus(@ModelAttribute("user") UserDTO userDTO){
+        return "/manager/project-status";
+    }
 
+    @RequestMapping("/account")
+    public String getAccountInfo(@ModelAttribute("user") UserDTO userDTO){
+        return "account";
+    }
+
+    @RequestMapping("/create-task")
+    public String getTask(@ModelAttribute("user") UserDTO userDTO){
+        return "/task/create-task";
+    }
 
 
 }
