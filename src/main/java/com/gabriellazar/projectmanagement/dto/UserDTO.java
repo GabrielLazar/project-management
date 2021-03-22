6e8fad8cbfc925ce9ff620b6334f9ccf77b2f0d9
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2021-03-21 22:30:14 -0500

    added validations to dto

--- src/main/java/com/gabriellazar/projectmanagement/dto/UserDTO.java
+++ src/main/java/com/gabriellazar/projectmanagement/dto/UserDTO.java
@@ -6,8 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 
@@ -18,18 +17,27 @@ public class UserDTO {
 
     private Long id;
     @NotBlank
-
+    @Size(min = 2, max = 30)
     private String firstName;
     @NotBlank
-    @Size
+    @Size(min = 2, max = 30)
     private String lastName;
     @NotBlank
     private String userName;
+    @NotBlank
+    @Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}")
+    @Size(min = 8)
     private String password;
+    @NotBlank
+    @Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}")
+    @Size(min = 8)
     private String confirmPassword;
+    @NotBlank
+    @Email
     private String email;
+    @NotBlank
+    @Pattern(regexp = "^\\d{10}$")
     private String phoneNumber;
-
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
     private boolean enabled;
