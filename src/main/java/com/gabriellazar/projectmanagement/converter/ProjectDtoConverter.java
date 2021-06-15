34d1e08ae2aac56b9363181e5a65664b9d114757
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2021-06-14 22:58:51 -0500

    update to project converter

--- src/main/java/com/gabriellazar/projectmanagement/converter/ProjectDtoConverter.java
+++ src/main/java/com/gabriellazar/projectmanagement/converter/ProjectDtoConverter.java
@@ -1,4 +1,22 @@
 package com.gabriellazar.projectmanagement.converter;
 
-public class ProjectDtoConverter {
+import com.gabriellazar.projectmanagement.dto.ProjectDTO;
+import com.gabriellazar.projectmanagement.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationPropertiesBinding
+public class ProjectDtoConverter implements Converter<String, ProjectDTO> {
+
+    @Autowired
+    ProjectService projectService;
+
+    @Override
+    public ProjectDTO convert(String source) {
+        Long id = Long.valueOf(source);
+        return projectService.findProjectById(id);
+    }
 }
