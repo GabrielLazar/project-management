b1a9c37761d88cd960d77af372eafb1356971cc9
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2021-07-01 22:39:16 -0500

    created Task entity

---
+++ src/main/java/com/gabriellazar/projectmanagement/entity/Task.java
@@ -0,0 +1,33 @@
+package com.gabriellazar.projectmanagement.entity;
+
+import com.gabriellazar.projectmanagement.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tasks")
+public class Task extends BaseEntity {
+
+    private String taskSubject;
+    private String taskDetail;
+
+    @Enumerated(EnumType.STRING)
+    private Status taskStatus;
+
+    private LocalDate assignedDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User assignedUser;
+}
