8d31421fab11303a9dd23b15ce5d73509df16b35
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2021-07-04 13:11:36 -0500

    added logs to projectService

--- src/main/java/com/gabriellazar/projectmanagement/services/impl/ProjectServiceImpl.java
+++ src/main/java/com/gabriellazar/projectmanagement/services/impl/ProjectServiceImpl.java
@@ -1,24 +1,23 @@
 package com.gabriellazar.projectmanagement.services.impl;
 
-import ch.qos.logback.core.joran.util.beans.BeanUtil;
 import com.gabriellazar.projectmanagement.dto.ProjectDTO;
 import com.gabriellazar.projectmanagement.entity.Project;
 import com.gabriellazar.projectmanagement.enums.Status;
 import com.gabriellazar.projectmanagement.mapper.MapperUtil;
 import com.gabriellazar.projectmanagement.repository.ProjectRepository;
 import com.gabriellazar.projectmanagement.services.ProjectService;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
+import lombok.extern.slf4j.Slf4j;
 import java.util.Optional;
 
 
 @Service
+@Slf4j
 public class ProjectServiceImpl implements ProjectService {
 
     private ProjectRepository projectRepository;
@@ -33,8 +32,14 @@ public class ProjectServiceImpl implements ProjectService {
     @Override
     public Page<ProjectDTO> findAllPageableProjects(int pageNo, int pageSize) {
         Pageable pageable = PageRequest.of(pageNo-1,pageSize);
-        return projectRepository.findAllByOrderByLastUpdateDateTimeDesc(pageable)
-                .map(project -> mapperUtil.convertToDTO(project,new ProjectDTO()));
+        Page<ProjectDTO> pagesProjects = null;
+        try{
+            pagesProjects = projectRepository.findAllByOrderByLastUpdateDateTimeDesc(pageable)
+                            .map(project -> mapperUtil.convertToDTO(project,new ProjectDTO()));
+        } catch (Exception e){
+            log.error("Exception in getting all pageable Projects :: {}",e);
+        }
+        return pagesProjects;
     }
 
     @Override
@@ -45,28 +50,53 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public void saveProject(ProjectDTO projectDTO) {
-        projectDTO.setProjectStatus(Status.OPEN);
-        projectRepository.saveAndFlush(mapperUtil.convertToEntity(projectDTO, new Project()));
+        log.info("Starting to save project :: {}",projectDTO);
+        try{
+                projectDTO.setProjectStatus(Status.OPEN);
+                projectRepository.saveAndFlush(mapperUtil.convertToEntity(projectDTO, new Project()));
+        } catch (Exception e){
+            log.error("Exception in saving project :: {}", projectDTO);
+            log.error("Exception saving project :: {}", e);
+        }
+        log.info("{} project was saved",projectDTO);
     }
 
     @Override
     public void deleteProject(Long id) {
-        projectRepository.deleteById(id);
+        try{
+            projectRepository.deleteById(id);
+        } catch (Exception e){
+            log.error("Exception in deleting the project by id {} :: {} ",id,e);
+        }
+       log.info("Successfully deleted project with id :: {}",id);
     }
 
     @Override
     public ProjectDTO findProjectById(Long id) {
-        return mapperUtil.convertToDTO(projectRepository.findById(id).get(),new ProjectDTO());
+        ProjectDTO projectDTO = null;
+        try{
+            projectDTO = mapperUtil.convertToDTO(projectRepository.findById(id).get(),new ProjectDTO());
+        } catch (Exception e){
+            log.error("Exception in finding project by id {} :: {}",id,e);
+        }
+        return projectDTO;
     }
 
     @Override
     public ProjectDTO updateProject(Long id, ProjectDTO projectDTO) {
-        Project existingProject = projectRepository.findById(id).get();
-        Project currentProject = mapperUtil.convertToEntity(projectDTO, new Project());
-        currentProject.setId(id);
-        currentProject.setInsertDateTime(existingProject.getInsertDateTime());
-        currentProject.setInsertUserId(existingProject.getInsertUserId());
-        projectRepository.save(currentProject);
+        try{
+            log.info("Updatind project with id {} :: {}",id,projectDTO);
+            Project existingProject = projectRepository.findById(id).get();
+            Project currentProject = mapperUtil.convertToEntity(projectDTO, new Project());
+            currentProject.setId(id);
+            currentProject.setInsertDateTime(existingProject.getInsertDateTime());
+            currentProject.setInsertUserId(existingProject.getInsertUserId());
+            projectRepository.save(currentProject);
+            log.info("Successfully updated project with id {} :: {}", id,currentProject);
+        } catch (Exception e){
+            log.error("Exception in updating project {} :: {}", id,projectDTO);
+            log.error("Exception was ::{}",e);
+        }
         return findProjectById(id);
     }
 }
